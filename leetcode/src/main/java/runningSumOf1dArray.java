758e41f467c529cf9409201eaadba7a19c3174e9
Kalyanasundharam Ramachandran <kalyanasundharamramachandran@Kalyanasundharams-MacBook-Pro.local>
2022-09-07 21:26:53 -0700

    day 1 - easy

---
+++ leetcode/src/main/java/runningSumOf1dArray.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import java.util.Arrays;
+
+public class runningSumOf1dArray {
+    //my solution
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        int sum = 0;
+        for(int i = 0; i < nums.length ; i++) {
+            sum += nums[i];
+            result[i] = sum;
+        }
+        return result;
+    }
+
+    //givenSolution
+    public int[] runningSumSolution2(int[] nums) {
+        int[] result = new int[nums.length];
+            result[0] = nums[0];
+        for(int i = 1; i < nums.length ; i++) {
+            result[i] = result[i-1] + nums[i];
+        }
+        return result;
+    }
+}
