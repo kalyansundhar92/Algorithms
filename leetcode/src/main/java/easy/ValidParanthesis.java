1090de8fa60b350e0bf9d94ab78658f008c9103a
Kalyan <kalyansundhar92@gmail.com>
2022-10-04 18:10:13 -0700

    Easy 1

---
+++ leetcode/src/main/java/easy/ValidParanthesis.java
@@ -0,0 +1,30 @@
+package main.java.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParanthesis {
+
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+            for(int i = 0 ; i < s.length() ; i++) {
+                if(s.charAt(i) == '(') {
+                    stack.push(')');
+                } else if (s.charAt(i) == '{' ) {
+                    stack.push('}');
+                } else if (s.charAt(i) == '[') {
+                    stack.push(']');
+                } else {
+                    if (!stack.isEmpty() && stack.pop() == s.charAt(i)) {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return stack.empty() && true;
+        }
+    }
+}
--- leetcode/src/main/java/easy/runningSumOf1dArray.java
+++ leetcode/src/main/java/easy/runningSumOf1dArray.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.easy;
 
 import java.util.Arrays;
 
