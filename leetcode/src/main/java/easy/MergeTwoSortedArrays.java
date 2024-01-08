1348412403b2bed815a4850df6c45fe76ec01f15
Kalyan <kalyansundhar92@gmail.com>
2024-01-08 12:20:16 -0700

    Merge two sorted arrays

---
+++ leetcode/src/main/java/easy/MergeTwoSortedArrays.java
@@ -0,0 +1,45 @@
+package main.java.easy;
+
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class MergeTwoSortedArrays {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        Queue<Integer> firstQueue = new ArrayDeque<>();
+        for(int i=0 ; i<m ; i++) {
+            firstQueue.add(nums1[i]);
+        }
+
+        Queue<Integer> secondQueue = new ArrayDeque<>();
+        for(int j=0; j<n ; j++) {
+            secondQueue.add(nums2[j]);
+        }
+
+        int k = 0;
+        while(firstQueue.size() > 0 || secondQueue.size() > 0) {
+            if(firstQueue.isEmpty()) {
+                nums1[k] = secondQueue.poll();
+            } else if(secondQueue.isEmpty()) {
+                nums1[k] = firstQueue.poll();
+            } else {
+                if(firstQueue.element() < secondQueue.element()) {
+                    nums1[k] = firstQueue.poll();
+                } else {
+                    nums1[k] = secondQueue.poll();
+                }
+            }
+            ++k;
+        }
+    }
+}
+
