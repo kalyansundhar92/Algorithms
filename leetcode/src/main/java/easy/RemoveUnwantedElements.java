8d0819ec72078d7013a213ed837511e5c42397b2
Kalyan <kalyansundhar92@gmail.com>
2024-01-08 13:01:54 -0700

    Easy-01/08[Remove unwanted elements]

---
+++ leetcode/src/main/java/easy/RemoveUnwantedElements.java
@@ -0,0 +1,25 @@
+package main.java.easy;
+
+public class RemoveUnwantedElements {
+
+    public static void main(String[] args) {
+        Solution1 solution1 = new Solution1();
+        int[] nums = {3,3};
+
+        solution1.removeElement(nums, 3);
+    }
+}
+
+class Solution1 {
+    public int removeElement(int[] nums, int val) {
+            int index=0;
+            for(int i = 0 ; i < nums.length ; i++) {
+                if(nums[i] != val) {
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+
+            return index;
+    }
+}
